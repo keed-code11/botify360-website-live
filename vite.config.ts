6655f92f629cae742fec4d8dbb3f376cb572b622
keed-code11 <keedcode@gmail.com>
2026-05-16 21:22:27 +0200

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// other imports, e.g.:
+// import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 3000,
-    strictPort: true,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  // your config
 })
